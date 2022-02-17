753b932f9d941a547d55a2abf33686a00aae6f5b
ductc241 <tacongduc123@gmail.com>
2022-02-17 21:40:18 +0700

    Coffee Website - Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,15 +31,33 @@ window.addEventListener('scroll', () => {
 })
 
 /*=============== MIXITUP FILTER PRODUCTS ===============*/
+let mixerProducts = mixitup('.products__content', {
+    selectors: {
+        target: '.products__card'
+    },
+    animation: {
+        duration: 300
+    }
+})
 
 
 /* Default filter products */ 
-
+mixerProducts.filter('.delicacies')
 
 /* Link active products */ 
+const linkProducts = document.querySelectorAll('.products__item')
 
+linkProducts.forEach(link => link.addEventListener('click', function() {
+    linkProducts.forEach(l => l.classList.remove('active-product'))
+    this.classList.add('active-product')
+}))
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+window.addEventListener('scroll', () => {
+    const scrollUp = document.getElementById('scroll-up');
+    (this.scrollY >= 350) 
+        ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+})
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
